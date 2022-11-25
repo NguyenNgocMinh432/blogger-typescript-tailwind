2ab6f8a61e8f8e44bef6f0dfb5bc1989ac5d7fa1
NguyenNgocMinh <minhdev.mochi@gmail.com>
2022-11-26 01:15:13 +0700

    firt commit

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -3,5 +3,4 @@ import React from "react";
 const Header = () => {
 	return <div className="w-screen h-[40px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>;
 };
-
 export default Header;
